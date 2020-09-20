02aa979cd32878583d4a01e8fa8b012b865bb59d
Tobias Sol <insightfortheblind@gmail.com>
2020-09-20 18:31:58 -0500

    Fixed satellite list render

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Satellite } from './satellite';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'orbit-report';
+  displayList: Satellite[];
+  sourceList: Satellite[];
+  
+constructor() {
+    this.sourceList = [];
+    this.displayList = []
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+ 
+    window.fetch(satellitesUrl).then(function(response) {
+       response.json().then(function(data) {
+          const {satellites} = data;
+          for (let o of satellites){
+            this.sourceList.push(
+               new Satellite(
+                  o.name,
+                  o.type,
+                  o.launchDate,
+                  o.orbitType,
+                  o.operational
+               )               
+            )            
+         }
+         this.displayList = this.sourceList.slice(0);
+       }.bind(this));
+    }.bind(this));
+ }
+ 
 }
